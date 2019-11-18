c498af11447c9bee871f4faa3edfe511881117b5
Cinzya <cynti-ebert@web.de>
2019-11-18 20:21:15 +0100

    domain state

--- src/components/input-field.js
+++ src/components/input-field.js
@@ -1,24 +1,35 @@
 import React, { Component } from 'react';
-
-const getData = (domain) => {
-    fetch("https://domain-availability-api.whoisxmlapi.com/api/v1?apiKey=at_XtU8CpRcPmD7AX6RWswtOOK0voVgH&domainName=" + domain).then(response => {
-        return response.json();
-    })
-        .then(responseData => {
-            console.log(responseData);
-        });
-};
-
+import { TextInput } from 'react-native-paper';
 
 class InputField extends Component {
+    constructor(props) {
+        super(props);
+        state = {
+            domain: ''
+        };
+    }
+
+    handleDomain = e => this.setState({ domain: e.target.value })
+    getData() {
+        fetch("https://domain-availability-api.whoisxmlapi.com/api/v1?apiKey=at_XtU8CpRcPmD7AX6RWswtOOK0voVgH&domainName=" + {domain}.then(response => {
+            return response.json();
+        })
+            .then(responseData => {
+                console.log(responseData);
+            });
+    };
+
     render() {
-        return(
+        const { domain } = this.state;
+        return (
             <div className="domain-eingabe">
-                <input type="text" id="eingabefeld" placeholder="Geben Sie hier Ihre Wunschdomain ein"/>
-                <button onClick={getData} className="button">Hinzufügen</button>
+                <input type="text" id="eingabefeld" placeholder="Geben Sie hier Ihre Wunschdomain ein"
+                       value={this.state.text} onChangeText={text => this.setState({ text })}/>
+                <button onClick={this.getData} className="button">Hinzufügen</button>
             </div>
         );
     };
-}
 
+}
 export default InputField;
+
