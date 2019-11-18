79840e64cb552d1c6b97b63a498c28c743741b16
annadoerr <doerranna@t-online.de>
2019-11-18 20:19:51 +0100

    sw_prototype final

--- sw_prototype/sw.js
+++ sw_prototype/sw.js
@@ -4,7 +4,7 @@ self.addEventListener('install', function(event) {
         return cache.addAll([
           '/sw_prototype/',
           '/sw_prototype/index.html',
-          '/sw_prototype/style.css',
+          '/sw_prototype/css/style.css',
           '/sw_prototype/app.js',
           '/sw_prototype/image-list.js',
           '/sw_prototype/img/cat1.jpg',
@@ -18,10 +18,26 @@ self.addEventListener('install', function(event) {
   });
 
   /*Holt Dateien vom Cache, wenn Internet nicht geht */
-  this.addEventListener('fetch', function(event) {
-    event.respondWith(
-      caches.match(event.request).catch(function() {
-        return fetch(event.request);
-      })
-    );
+  self.addEventListener('fetch', function(event) {
+    event.respondWith(caches.match(event.request).then(function(response) {
+      // caches.match() always resolves
+      // but in case of success response will have value
+      if (response !== undefined) {
+        return response;
+      } else {
+        return fetch(event.request).then(function (response) {
+          // response may be used only once
+          // we need to save clone to put one copy in cache
+          // and serve second one
+          let responseClone = response.clone();
+          
+          caches.open('v1').then(function (cache) {
+            cache.put(event.request, responseClone);
+          });
+          return response;
+        }).catch(function () {
+          return caches.match('/sw_prototype/img/cat1.jpg');
+        });
+      }
+    }));
   });
