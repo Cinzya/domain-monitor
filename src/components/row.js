2e36909e0d0a5e3e66a1a221799afa7ac64d601d
annadoerr <doerranna@t-online.de>
2019-12-09 13:19:57 +0100

    table_sort

--- src/components/row.js
+++ src/components/row.js
@@ -1,22 +1,13 @@
 import React from 'react';
 import zahnrad from "../img/zahnrad.svg";
 import muelleimer from "../img/muelleimer.svg";
-import burgermenu from "../img/burgermenu.svg";
 
 export const Row = (props) => (
 
     <tr key={props.id}>
 
-        <td> 
-        <div 
-        //Macht das Burgermenü draggable
-        className="drag"
-        draggable={true}>
-        <img src={burgermenu}/> 
-        </div> </td>
-    
         <td><img src={zahnrad} /></td>
-        <td>{props.url}</td>
+        <td> {props.url}</td>
         <td><span className={props.availability}>{props.availability}</span></td>
         <td>{props.time}</td>
         <td></td>
