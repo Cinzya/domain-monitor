01fedf8f30d55271d9dfc05ad460c68f9951eb28
Cinzya <cynti-ebert@web.de>
2019-12-14 03:04:23 +0100

    Tests in mehrere Dateien aufgeteilt

--- src/App.test.js
+++ src/App.test.js
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { shallow, mount, render } from 'enzyme'; // Einbinden Enzyme
 import { InputField } from './components/input-field';
-import Intro from './components/intro'; // hinzugefügt
-import List from './components/list'; // hinzugefügt
-import Navigation from './components/navigation'; // hinzugefügt
 
 //const wrapper = shallow(<Foo />);
 
@@ -15,34 +12,12 @@ it('renders without crashing', () => {
   ReactDOM.unmountComponentAtNode(div);
 });
 
-//Testing React Components
-describe('InputField', () => {
-  it('should render correctly in "debug" mode', () => {
-    const component = shallow(<InputField debug />);
-    expect(component).toMatchSnapshot();
-  });
-});
 
-describe('Intro', () => {
-  it('should render correctly in "debug" mode', () => {
-    const component = shallow(<Intro debug />);
-    expect(component).toMatchSnapshot();
-  });
-});
 
-describe('List', () => {
-  it('should render correctly in "debug" mode', () => {
-    const component = shallow(<List debug />);
-    expect(component).toMatchSnapshot();
-  });
-});
 
-describe('Navigation', () => {
-  it('should render correctly in "debug" mode', () => {
-    const component = shallow(<Navigation debug />);
-    expect(component).toMatchSnapshot();
-  });
-});
+
+
+
 
 
 
