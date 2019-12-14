afb077318a7186cb46c2c4755af14ec86957adc6
Cinzya <cynti-ebert@web.de>
2019-12-14 15:17:15 +0100

    alt-Tags hinzugefuegt und nicht verwendete Dateien entfernt,

--- src/components/row.js
+++ src/components/row.js
@@ -4,11 +4,11 @@ import muelleimer from "../img/muelleimer.svg";
 
 export const Row = (props) => (
     <tr key={props.id}>
-        <td><img src={zahnrad} /></td>
+        <td><img src={zahnrad} alt="Zahnradbutton für Einstellungen"/></td>
         <td>{props.url}</td>
         <td><span className={props.availability}>{props.availability}</span></td>
         <td>{props.time}</td>
         <td></td>
-        <td><img src={muelleimer} /></td>
+        <td><img src={muelleimer} alt="Löschbutton"/></td>
     </tr>
 );
