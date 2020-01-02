eabfd49fd5b35236cb280368c0fc123b956f223c
Antonia Miehling <antoniamiehling@web.de>
2020-01-02 15:52:19 +0100

    row.test.js works now, installed @babel/runtime + fixed import-issues

--- src/components/row.test.js
+++ src/components/row.test.js
@@ -1,7 +1,8 @@
 import { shallow } from 'enzyme';
 import React from 'react';
-import Row from './row';
+import { Row } from './row';
 
 it('expect to render Row component', () => {
-    expect(shallow(<Row />)).toMatchSnapshot();
+    const component = shallow(<Row debug />);
+    expect(component).toMatchSnapshot();
 });
