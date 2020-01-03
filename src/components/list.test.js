12fb54801d71a3c35ec94b1b771930c03e8d171b
unknown <sophiapfahler@web.de>
2020-01-03 15:41:55 +0100

    React Test Renderer installiert

--- src/components/list.test.js
+++ src/components/list.test.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import {shallow} from "enzyme";
 import List from "./list";
+import * as renderer from 'react-test-renderer';
 
 describe('List', () => {
     it('should render correctly in "debug" mode', () => {
         const component = shallow(<List debug />);
         expect(component).toMatchSnapshot();
     });
+
+    it('renders correctly', () => {
+        const list = renderer.create(<List />).toJSON();
+        expect(list).toMatchSnapshot();
+    });
+
+   
     
 });
