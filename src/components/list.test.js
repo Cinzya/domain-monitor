11f554a394a7b95ad71c5dec66c4b14bb1d1c396
annadoerr <doerranna@t-online.de>
2020-01-04 14:37:13 +0100

    testTest

--- src/components/list.test.js
+++ src/components/list.test.js
@@ -1,10 +1,19 @@
 import React from 'react';
-import {shallow} from "enzyme";
+/* import {shallow} from "enzyme"; */
 import List from "./list";
 
 describe('List', () => {
     it('should render correctly in "debug" mode', () => {
         const component = shallow(<List debug />);
         expect(component).toMatchSnapshot();
+    });
+
+    //Test addData Function
+    it('should add Domain data to the table', () => {
+        //bei Arrays: expect().toContain();
+        //Async Data:(Minute 24:29): expect.assertions(1);
+        //return functions.fetchUser().then(data => {expect(data.name).toEqual('')})
+        const content = addData(toAdd);
+        expect(content).toEqual(responseData)
     });
 });
