e2bf2352f5f1cb805463adf3244222ef106d2496
Antonia Miehling <antoniamiehling@web.de>
2020-01-08 15:52:46 +0100

    deleteEvent gets called on mülleimer-Click

--- src/components/row.test.js
+++ src/components/row.test.js
@@ -1,8 +1,20 @@
 import { shallow } from 'enzyme';
 import React from 'react';
 import { Row } from './row';
+import deleteEvent from './list';
 
-it('expect to render Row component', () => {
+
+describe ('Row', () => {
+ it('expect to render Row component', () => {
     const component = shallow(<Row debug />);
     expect(component).toMatchSnapshot();
+});
+
+it('is expected to call function delete on click on the mülleimer', () => {
+    const component = shallow(<Row />);
+    const muelleimer = component.find('form');
+    muelleimer.simulate('click');
+    expect(deleteEvent).toBeCalled;
+})
+
 });
