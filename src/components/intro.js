98324583b3d02c3faf65fdbb682004a1daa7c90a
unknown <sophiapfahler@web.de>
2020-01-08 16:46:39 +0100

    added 'paragraph test'

--- src/components/intro.js
+++ src/components/intro.js
@@ -6,13 +6,9 @@ class Intro extends Component {
             <div className="column-left">
                
                 <h2>Was ist ein <br></br> Domain Monitor?</h2>
-                <p>Domain Monitor ist ein Dienst, mit dem Benutzer den Registrierungsstatus von Domains überwachen
-                    können. Der Service benachrichtigt Sie (per E-Mail, wenn überwachte Domains kurz vor dem Ablauf
-                    stehen und ihren Status ändern. Mit Domain Monitor können Sie Ihre Domains im Auge behalten ... oder
-                    die anderer! Ablaufdaten, Statusänderungen - geben Sie einfach die Domainnamen ein, die Sie
-                    überwachen möchten, und das Tool benachrichtigt Sie laufend über wichtige Änderungen bei der
-                    Registrierung. Dieser Dienst kann sechs wichtige TLDs überwachen: .com, .org,, info, .us, .net und
-                    .biz.</p>
+                <p>Der Domain Monitor ist ein Dienst, mit dem Benutzer den Registrierungsstatus von Domains überwachen
+                    können. Mit Domain Monitor können Sie Ihre Domains im Auge behalten ... oder die anderer! Geben Sie einfach die Domainnamen ein, die Sie
+                    überwachen möchten, und das Tool teilt Ihnen den Verfügbarkeitsstatus mit und wann Sie diesen zuletzt überprüft haben. Sie können die Verwaltungsliste je nach Präferenz sortieren.</p>
                 
 
             </div>
--- src/components/intro.test.js
+++ src/components/intro.test.js
@@ -8,5 +8,13 @@ describe('Intro', () => {
         expect(component).toMatchSnapshot();
     });
 
-    //it('contains a paragraph that explains how the DomainMonitor works', () => { };
+    it('contains a paragraph that explains how the DomainMonitor works', () => {
+        const component = shallow(<Intro />);
+        const expectedOutput =
+        '<div class="column-left"><h2>Was ist ein <br/> Domain Monitor?</h2><p>Der Domain Monitor ist ein Dienst, mit dem Benutzer den Registrierungsstatus von Domains überwachen können. Mit Domain Monitor können Sie Ihre Domains im Auge behalten ... oder die anderer! Geben Sie einfach die Domainnamen ein, die Sie überwachen möchten, und das Tool teilt Ihnen den Verfügbarkeitsstatus mit und wann Sie diesen zuletzt überprüft haben. Sie können die Verwaltungsliste je nach Präferenz sortieren.</p></div>';
+        const realOutput = component.find('div.column-left').html();
+        expect(realOutput).toEqual(expectedOutput);
+    });
+
+   
 });
