67dc0bdb7bbff3d006637fd255f269285d535fb5
yingyingxx <yingying.xu17@gmail.com>
2020-01-10 18:16:15 +0100

    testings onclick

--- src/components/row.test.js
+++ src/components/row.test.js
@@ -3,7 +3,21 @@ import React from 'react';
 import Row from './row';
 import ReactDOM from 'react-dom';
 import {cleanup} from '@testing-library/react';
+import { render} from "react-dom";
+import { act } from "react-dom/test-utils";
 import {create} from 'react-test-renderer';
+import deleteEvent from './list';
+
+
+let container = null;
+
+beforeEach(() => {
+  // DOM element als render Ziel festlegen
+  container = document.createElement("div");
+  //container hängt an document dran 
+  document.body.appendChild(container);
+  });
+
 
 afterEach(cleanup);
 
@@ -13,27 +27,40 @@ describe('Row', () => {
         expect(component).toMatchSnapshot();
     })
     it("renders without crashing", () => {
-    const div = document.createElement("div");
-    ReactDOM.render(<Row/>, div)
+    ReactDOM.render(<Row/>, container)
     })
     it("should find muell image element in the DOM", () => {
         const wrapper = shallow(<Row text="test"/>)
         expect(wrapper.find("#muell")).toHaveLength(1);
     })
-
     it("should find muell image element with className test in the DOM",() => {
         const wrapper = shallow(<Row className="test" text="test"/>)
         expect(wrapper.find("#muell.test")).toHaveLength(1);
     })
-    it('should call deleteEvent function wrapper click', () => {
-        const mockCallBackClick = jest.fn();
-        const wrapper = shallow (<Row onClick={mockCallBackClick} className="test" text="test"/>);
-        wrapper.find("input").simulate("click");
-        expect(mockCallBackClick.mock.calls.length).toEqual(1);
+      it("changes value when clicked", () => {
+        const onClick = jest.fn();
+        act(() => {
+          ReactDOM.render(<Row onClick={onClick}/>, container);
+        });
+      
+        const wrapper = container.querySelector("#muell.test");
+        expect(onClick).toHaveBeenCalledTimes(0);
+        //expect(wrapper.state).toBe({props.delete});
+      
+        act(() => {
+          wrapper.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+        });
+      
+        expect(onClick).toHaveBeenCalledTimes(1);
+        //expect(button.innerHTML).toBe("");
+      });
 
-
-        //wrapper.find('muell').simulate('click');
-        //expect(deleteEvent).toHaveBeenCalled();
+      it('should call deleteEvent function wrapper click', () => {
+        const wrapper = shallow (<Row />);
+        const muelleimer = wrapper.find("form");
+        muelleimer.simulate("click");
+        expect(deleteEvent).tobeCalled;
       })
+      }
 
-});
+);
