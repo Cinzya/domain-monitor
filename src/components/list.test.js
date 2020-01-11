5d249485d67b1bdbeac6dca9b91054300673b538
annadoerr <doerranna@t-online.de>
2020-01-11 16:58:42 +0100

    testings with addDataTest

--- src/components/list.test.js
+++ src/components/list.test.js
@@ -173,6 +173,36 @@ describe('List', () => {
     
   })
 
+    //Test addData Function
+    it('should add domain API Data into the state', () => {
+      const component = mount(<List />);
+      const instance = component.instance();
+      //Leerer State
+      /* expect(instance.state.domainName).toBe(""); */
+  
+      instance.setState(() => {
+        domains: [
+          {
+            DomainInfo: {
+              domainName: "bar.com",
+              domainAvailability: "UNAVAILABLE"
+            },
+            id: 1,
+          },
+          {
+            DomainInfo: {
+              domainName: "google.com",
+              domainAvailability: "UNAVAILABLE"
+            },
+            id: 2,
+          }
+        ]
+      })
+  
+      instance.forceUpdate();
+      expect(instance.state).toBe()
+    });
+
 });
 
 
