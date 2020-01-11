17a34ceda3095f0d2755319bf0429b67f70eb6bf
annadoerr <doerranna@t-online.de>
2020-01-11 15:26:45 +0100

    Stryker_CoverageTest

--- src/components/list.test.js
+++ src/components/list.test.js
@@ -1,16 +1,40 @@
 import React from 'react';
-import Enzyme, {shallow, render, mount} from 'enzyme';
+import Enzyme, { shallow, render, mount } from 'enzyme';
 import List from "./list";
 
 describe('List', () => {
-    it('should render correctly in "debug" mode', () => {
-        const component = shallow(<List debug />);
-        expect(component).toMatchSnapshot();
-    });
+  it('should render correctly in "debug" mode', () => {
+    const component = shallow(<List debug />);
+    expect(component).toMatchSnapshot();
+  });
 
-    //Test addData Function
-    it('should add domain API Data into the state', () => {
-        const component = shallow(<List/>);
-        expect(component.instance().state.domainName).toBe("");
-    });
+  //Test addData Function
+  it('should add domain API Data into the state', () => {
+    const component = mount(<List />);
+    const instance = component.instance();
+    //Leerer State
+    /* expect(instance.state.domainName).toBe(""); */
+
+    instance.setState(() => {
+      domains: [
+        {
+          DomainInfo: {
+            domainName: "bar.com",
+            domainAvailability: "UNAVAILABLE"
+          },
+          id: 1,
+        },
+        {
+          DomainInfo: {
+            domainName: "google.com",
+            domainAvailability: "UNAVAILABLE"
+          },
+          id: 2,
+        }
+      ]
+    })
+
+    instance.forceUpdate();
+    expect(instance.state).toBe()
+  });
 });
