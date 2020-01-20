4bfbceb52ae6b5bc1ac727b8c8a92b3d33ab581b
Cinzya <cynti-ebert@web.de>
2020-01-20 01:21:38 +0100

    Formatierung verbessert

--- src/components/input-field.js
+++ src/components/input-field.js
@@ -1,11 +1,22 @@
 import React from 'react';
 
 export const InputField = (props) => (
-    <form className="domain-eingabe"
-        onSubmit={(event)=> {props.Submit(event)}}>
+    <form
+        className="domain-eingabe"
+        onSubmit={(event)=> {props.Submit(event)}}
+    >
 
-        <input type="text" id="eingabefeld" placeholder="Geben Sie hier Ihre Wunschdomain ein"
-               value={props.domainName} onChange={props.changeDomain}/>
-        <input type="submit" className="button" value="Hinzufügen"/>
+        <input
+            type="text" id="eingabefeld"
+            placeholder="Geben Sie hier Ihre Wunschdomain ein"
+            value={props.domainName}
+            onChange={props.changeDomain}
+        />
+
+        <input
+            type="submit"
+            className="button"
+            value="Hinzufügen"
+        />
     </form>
 );
--- src/components/row.js
+++ src/components/row.js
@@ -6,13 +6,19 @@ export const Row = (props) => (
    
     <tr key={props.id}>
         <td><img src={zahnrad} alt="Zahnradbutton für Einstellungen"/></td>
-        <td id= "url">{props.url}</td>
+        <td id="url">{props.url}</td>
         <td><span className={props.availability}>{props.availability}</span></td>
-        <td id= "checked">{props.checked}</td>
+        <td id="checked">{props.checked}</td>
         <td id="added">{props.added}</td>
         <td>
             <form >
-                <input id="muell" className="test" onClick={props.delete} type="image" src={muelleimer} alt="Mülleimer als Button"/>
+                <input
+                    id="muell"
+                    className="test"
+                    onClick={props.delete}
+                    type="image"
+                    src={muelleimer}
+                    alt="Mülleimer als Button"/>
             </form>
             </td>
     </tr>
